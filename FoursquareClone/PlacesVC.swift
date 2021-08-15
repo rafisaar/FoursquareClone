095289df739bc5aeae65dc84951ccbd87716b4eb
Syncopa Productions <info@syncopa.net>
2021-08-15 16:36:51 +0300

    Get info from Parse

--- FoursquareClone/PlacesVC.swift
+++ FoursquareClone/PlacesVC.swift
@@ -9,9 +9,12 @@
 import UIKit
 import Parse
 
-class PlacesVC: UIViewController {
+class PlacesVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var placeNameArray = [String]()
+    var placeIdArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,43 @@ class PlacesVC: UIViewController {
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
      
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getDataFromParse()
+        
+    }
+    
+    
+    func getDataFromParse() {
+        
+        let query = PFQuery(className: "Places")
+        query.findObjectsInBackground { (objects, error) in
+            if error != nil {
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+            } else {
+                
+                if objects != nil {
+                    
+                    self.placeIdArray.removeAll(keepingCapacity: false)
+                    self.placeNameArray.removeAll(keepingCapacity: false)
+                    
+                    for object in objects! {
+                        if let placeName = object.object(forKey: "name") as? String {
+                            if let placeId = object.objectId as? String {
+                                self.placeNameArray.append(placeName)
+                                self.placeIdArray.append(placeId)
+                                 
+                            }
+                        }
+                    }
+                    
+                    self.tableView.reloadData()
+                    
+                }
+                
+            }
+        }
     }
     
     @objc func addButtonClicked() {
@@ -30,15 +70,33 @@ class PlacesVC: UIViewController {
         
         PFUser.logOutInBackground { (error) in
             if error != nil {
-                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
-                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-                alert.addAction(okButton)
-                self.present(alert, animated: true, completion: nil)
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
             } else {
                 self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
             }
         }
         
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = placeNameArray[indexPath.row]
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeNameArray.count
+    }
+    
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+
+    }
+    
     
 }
